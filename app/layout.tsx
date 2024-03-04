24656a90605d702495f4d810e9a95d83756e931f
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-03-04 09:11:10 +0100

    feat: add layout with styled background and color theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-background container">
+      <body className="bg-background my-container">
         <Header />
         {children}
       </body>
