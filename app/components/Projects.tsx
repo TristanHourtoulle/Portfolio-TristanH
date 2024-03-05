86909a55649e015aad8b48885af00b86a1fe4202
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-03-05 14:17:04 +0100

    feat: add empty projects section

---
+++ app/components/Projects.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div className='mt-10'>
+        <h2 className='text-h2-fluid text-center text-paragraph poppins-semibold'>Projects</h2>
+    </div>
+  )
+}
+
+export default Projects
