233a74848a8cf7f88c2b4bb14e2f59a82eea287f
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-03-05 09:31:45 +0100

    feat: add new gradiant and primary color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      gradientColorStops: theme => ({
+        'blue-gradient': '#0081FF',
+      }),
+      linearGradientColors: theme => ({
+        'blue-gradient': ['to top', '#0081FF', '#FFFFFF'],
+      }),
       colors: {
         background: "#0D1520",
         backgroundPrimary: "#3B9EFF",
         navbar: "#104D87",
-        primary: "#3B9EFF",
+        primary: "#0081FF",
         secondary: "#104D87",
         paragraph: "#FFFFFF"
       },
