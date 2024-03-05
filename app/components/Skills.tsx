8d686f1ecad7997865fa12575966378239cc0d89
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-03-05 11:07:00 +0100

    feat: add skills sections

---
+++ app/components/Skills.tsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import Image from 'next/image'
+
+function logoWithText(imagePath: string, title: string) {
+    return (
+        <div className='mb-3 flex gap-3 items-center'>
+            <Image
+                src={imagePath}
+                alt={title}
+                width={25}
+                height={25}
+                className=''
+            />
+            <p className='text-paragraph text-p-fluid poppins-thin opacity-85'>{title}</p>
+        </div>
+    )
+}
+
+function logoNotSquareWithText(imagePath: string, title: string, height: number, width: number) {
+    return (
+        <div className='mb-3 flex gap-4 items-center'>
+            <Image
+                src={imagePath}
+                alt={title}
+                width={width}
+                height={height}
+                className=''
+            />
+            <p className='text-paragraph text-p-fluid poppins-thin opacity-85'>{title}</p>
+        </div>
+    )
+}
+
+const Skills = () => {
+  return (
+    <div className='mt-10'>
+        <h2 className='text-h2-fluid text-center text-paragraph poppins-semibold'>Skills</h2>
+
+        <div className='flex gap-5 justify-between mx-[10%] my-[5%]'>
+            <div>
+                <h3 className='text-h3-fluid text-paragraph poppins-regular'>Design</h3>
+                <ul className='mt-5'>
+                    <li>{logoNotSquareWithText("/images/figma.svg", "Figma", 25, 20)}</li>
+                    <li>{logoWithText("/images/photoshop.svg", "Photoshop")}</li>
+                </ul>
+            </div>
+
+            <div>
+                <h3 className='text-h3-fluid text-paragraph poppins-regular'>Front-end</h3>
+                <ul className='mt-5'>
+                    <li>{logoWithText("/images/nextjs.svg", "Next.js")}</li>
+                    <li>{logoWithText("/images/reactjs.svg", "React.js")}</li>
+                    <li>{logoWithText("/images/tailwind.svg", "Tailwind CSS")}</li>
+                </ul>
+            </div>
+
+            <div>
+                <h3 className='text-h3-fluid text-paragraph poppins-regular'>Back-end</h3>
+                <ul className='mt-5'>
+                    <li>{logoWithText("/images/nodejs.svg", "Node.js")}</li>
+                    <li>{logoWithText("/images/postgresql.svg", "Postgresql")}</li>
+                </ul>
+            </div>
+
+            <div>
+                <h3 className='text-h3-fluid text-paragraph poppins-regular'>Other</h3>
+                <ul className='mt-5'>
+                    <li>{logoWithText("/images/github.svg", "Git & Github")}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Skills
