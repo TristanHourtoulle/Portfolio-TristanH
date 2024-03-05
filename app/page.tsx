0ade35c9dc64bdc38ac131f9ec61cef405d6f274
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-03-05 09:32:10 +0100

    add animation to a block color in background

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,40 @@
 "use client"
 
 import Image from "next/image";
-import Link from "next/link";
 import { ButtonLogoText } from "@components/ButtonLogoText";
-import { Header } from "@components/Header";
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-
   const sendMail = `mailto:tristan.hourtoulle@epitech.eu`
 
   return (
-    <div className="main-content">
-      <div className="flex flex-col items-center justify-center md:flex-row md:justify-center md:items-center md:w-full">
-        <div className="order-2 md:order-1 md:mr-auto flex flex-col items-start">
-          <p className="text-primary font-medium text-xl">Hi! I'm -</p>
-          <h1 className="text-primary text-h1-fluid poppins-bold">Tristan Hourtoulle</h1>
-          <h2 className="text-paragraph text-h2-fluid poppins-bold">Web developer.</h2>
-          <div className="flex gap-7 mt-5">
-            <ButtonLogoText imagePath="/images/github.svg" title="GitHub" redirect="" />
-            <ButtonLogoText imagePath="/images/linkedin.svg" title="LinkedIn" redirect="" />
-            <ButtonLogoText imagePath="/images/email.svg" title="tristan.hourtoulle@epitech.eu" redirect={sendMail} />
+    <div>
+      <div className="absolute inset-0 flex items-center z-0 ml-20">
+        <div className={`rounded-full bg-blue-600 mix-blend-multiply filter blur-2xl opacity-10 w-[50%] h-[75%] zoom-in-out`}></div>
+      </div>
+      <div className="main-content sm:mx-auto relative overflow-hidden">
+        <div className="flex flex-col items-center justify-center md:flex-row md:justify-center md:items-center md:w-full">
+          <div className="order-2 md:order-1 md:mr-auto flex flex-col items-start relative z-10">
+            <p className="text-primary font-medium text-xl">Hi! I'm -</p>
+            <h1 className="text-h1-fluid poppins-bold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-blue-600 to-blue-500 relative z-10">Tristan Hourtoulle</h1>
+            <h2 className="text-paragraph text-h2-fluid poppins-bold">Web developer.</h2>
+            <div className="flex flex-wrap w-fit gap-5 md:flex-nowrap md:items-start md:gap-7 mt-5">
+              <ButtonLogoText imagePath="/images/github.svg" title="GitHub" redirect="" />
+              <ButtonLogoText imagePath="/images/linkedin.svg" title="LinkedIn" redirect="" />
+              <ButtonLogoText imagePath="/images/email.svg" title="tristan.hourtoulle@epitech.eu" redirect={sendMail} />
+            </div>
           </div>
-        </div>
-        <div className="order-1 md:order-2 md:flex md:justify-end">
-          <Image
-            src="/images/profil.svg"
-            alt="Tristan Hourtoulle"
-            width={500}
-            height={500}
-            className="responsive-image profil"
-          />
+          <div className="order-1 md:order-2 md:flex md:justify-end">
+            <Image
+              src="/images/profil.svg"
+              alt="Tristan Hourtoulle"
+              width={500}
+              height={500}
+              className="responsive-image profil"
+            />
+          </div>
         </div>
       </div>
     </div>
-
   );
 }
