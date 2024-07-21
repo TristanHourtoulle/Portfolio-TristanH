7fb52a2eee6bc95d78ea27e78fc97dea3afe2ecb
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-07-21 12:12:13 +0200

    feat: add optionnal color props in SectionTitle component

--- components/SectionTitle.tsx
+++ components/SectionTitle.tsx
@@ -1,11 +1,12 @@
 export type SectionTitleProps = {
   text: string;
+  color?: string;
 };
 
-export const SectionTitle = ({ text }: SectionTitleProps) => {
+export const SectionTitle = ({ text, color }: SectionTitleProps) => {
   return (
     <div className="w-full">
-      <h1 className="text-5xl poppins-bold text-primary">{text}</h1>
+      <h1 className={"text-5xl poppins-bold text-primary " + color}>{text}</h1>
     </div>
   );
 };
