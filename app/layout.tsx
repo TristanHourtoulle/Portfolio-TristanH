85e97defdc4b0c6118adaad3f6798a6a8ef54b3b
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-07-31 12:58:12 +0200

    fix: error with &apos;

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,10 +43,14 @@ export default function RootLayout({
         <meta charSet="UTF-8" />
         <style>
           @import
-          url(&quot;https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Righteous&display=swap&quot;);
+          url('https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Righteous&display=swap');
         </style>
       </head>
-      <body className={cn("w-full max-w-screen min-h-screen background-color")}>
+      <body
+        className={cn(
+          "w-full max-w-screen min-h-screen background-color scroll-smooth"
+        )}
+      >
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
