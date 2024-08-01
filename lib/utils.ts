b50be9129a55fa2349bc2c5fbc59ff877a009300
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-08-01 15:49:21 +0200

    fix: add comment & downArrow link to aboutMeSection div

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,10 +1,12 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+// Combine multiple class names (especially for Shadcnui)
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+// Scroll to a specific section smoothly
 export function scrollToSection(section: string) {
   const element = document.getElementById(section);
   if (element) {
@@ -12,10 +14,15 @@ export function scrollToSection(section: string) {
   }
 }
 
+// For DownArrow component (go to the first section)
 export function scrollToBottom() {
-  window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
+  const element = document.getElementById("aboutMeSection");
+  if (element) {
+    element.scrollIntoView({ behavior: "smooth" });
+  }
 }
 
+// For UpArrow component (go to the top of the page)
 export function scrollToTop() {
   window.scrollTo({ top: 0, behavior: "smooth" });
 }
