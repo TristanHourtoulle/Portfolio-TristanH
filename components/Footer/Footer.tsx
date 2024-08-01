fdbe421402ed2ca87e06a80000ab05b0fdc0f8b8
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-08-01 18:34:19 +0200

    feat: simplify footer comp

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,15 +2,15 @@ export type FooterProps = {};
 
 export const Footer = (props: FooterProps) => {
   return (
-    <div className="w-full">
-      <div className="w-full flex items-center justify-center mt-[5%]">
+    <div className="w-full mb-3">
+      <div className="w-full flex items-center justify-center">
         <div className="flex flex-col items-center justify-center">
-          <p className="outfit-medium text-sm md:text-lg text-primary text-center md:text-start">
-            © 2024 <span className="outfit-bold">Tristan Hourtoulle</span>
+          <p className="outfit-light text-sm text-primary text-center md:text-start">
+            © 2024 Tristan Hourtoulle - All Rights Reserved
           </p>
-          <p className="outfit-medium text-sm md:text-lg text-primary text-center md:text-start order-first">
+          {/* <p className="outfit-medium text-sm md:text-lg text-primary text-center md:text-start order-first">
             Designed and built with ❤️ by Tristan Hourtoulle
-          </p>
+          </p> */}
         </div>
       </div>
     </div>
