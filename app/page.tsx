88ddff04b8f74eca4971056f0d6da267ec72a162
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-08-01 18:34:00 +0200

    fix: responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,26 @@
 "use client";
 
 // Picture Import Section
-import FigmaLogo from "@/public/Figma.png";
-import GithubLogo from "@/public/Github.png";
-import GmailLogo from "@/public/gmail 1.png";
-import LinkedInLogo from "@/public/linkedin 1.png";
-import MaltLogoWhite from "@/public/Malt_logo_white 1.png";
-import MaltLogo from "@/public/Malt_logo_white.png";
-import NextjsLogo from "@/public/NextJS.png";
-import NodejsLogo from "@/public/NodeJS.png";
+import GithubLogoOrange from "@/public/developpeur 1.svg";
+import FigmaLogo from "@/public/Figma.svg";
+import GithubLogo from "@/public/Github.svg";
+import GmailLogo from "@/public/gmail (1) 1.svg";
+import LinkedInLogo from "@/public/linkedin (1) 1.svg";
+import MaltLogoWhite from "@/public/Malt_logo_white 1.svg";
+import MaltLogo from "@/public/Malt_logo_white.svg";
+import NextjsLogo from "@/public/NextJS.svg";
+import NodejsLogo from "@/public/NodeJS.svg";
 import ProfilPicture from "@/public/pictures/Photo de profil - TH.jpg";
-import ReactjsLogo from "@/public/ReactJS.png";
-import TailwindLogo from "@/public/Tailwind.png";
-import WavingHand from "@/public/WavingHand.png";
+import ReactjsLogo from "@/public/ReactJS.svg";
+import TailwindLogo from "@/public/Tailwind.svg";
+import WavingHand from "@/public/WavingHand.svg";
 
-import { DownArrow, UpArrow } from "@/components/cta/Arrow";
+import { ContactForm } from "@/components/contact/ContactForm";
+import { DownArrow } from "@/components/cta/Arrow";
+import { Edukai } from "@/components/projects/Edukai";
 import { ScrollingText } from "@/components/ScrollingText";
-import { SectionTitle } from "@/components/SectionTitle";
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 import { scrollToSection } from "@/lib/utils";
 import Head from "next/head";
 import Image from "next/image";
@@ -46,7 +49,7 @@ export default function Home() {
         <div className="px-[15%] flex flex-col lg:flex-row items-center justify-center lg:justify-between pb-[10%] w-full min-h-screen gap-1 md:gap-4 lg:gap-2">
           <div className="w-full lg:w-1/2 text-center lg:text-start text-primary flex flex-col items-center lg:items-start gap-1 lg:gap-4">
             <div className="flex flex-col items-center justify-center lg:items-start text-center lg:text-start w-full">
-              <div className="flex items-center gap-3">
+              <div className="flex items-center gap-1 md:gap-3">
                 <p className="outfit-medium md:text-2xl w-full mt-[5%] lg:mt-0">
                   Welcome!
                 </p>
@@ -110,17 +113,17 @@ export default function Home() {
               width={400}
               height={400}
               placeholder="blur"
-              className="rounded-[5%] transition-all mt-[15%] md:mt-[15%] lg:mt-0"
+              className="rounded-[5%] transition-all md:mt-[15%] lg:mt-0"
             />
           </div>
-          <div className="bottom-5 absolute left-[40%] lg:left-[50%] animate-light-bounce-little w-10 h-10">
+          <div className="bottom-2 md:bottom-5 absolute left-[40%] lg:left-[49%] w-10 h-10">
             <DownArrow location="#projectsSection" />
           </div>
         </div>
 
         {/* AboutMeSection */}
         <div
-          className="flex flex-col  gap-2 w-full pt-[5%]"
+          className="flex flex-col  gap-2 w-full pt-[20%] lg:pt-[5%]"
           id="aboutMeSection"
         >
           <div className="flex min-w-screen overflow-x-hidden">
@@ -129,7 +132,7 @@ export default function Home() {
             </div>
           </div>
 
-          <p className="outfit-regular text-secondary text-justify text-2xl px-[25%] pt-[3%]">
+          <p className="outfit-regular text-secondary text-justify text-lg lg:text-2xl px-[10%] md:px-[25%] pt-[3%]">
             Hi, I'm Tristan, a 22-year-old full stack developer from France. I
             specialize in{" "}
             <span className="outfit-bold">
@@ -139,10 +142,10 @@ export default function Home() {
             high-quality solutions tailored to clients' needs.
           </p>
 
-          <h3 className="text-4xl text-primary outfit-bold px-[25%] pt-[3%]">
+          <h3 className="text-4xl text-primary outfit-bold px-[10%] md:px-[25%] pt-[3%]">
             Skills
           </h3>
-          <div className="flex items-center justify-start gap-4 px-[25%]">
+          <div className="flex flex-wrap items-center justify-start gap-4 px-[10%] md:px-[25%]">
             <Image src={FigmaLogo} alt="Figma logo" width={30} height={30} />
             <Image src={NextjsLogo} alt="Next.js logo" width={50} height={50} />
             <Image
@@ -172,11 +175,14 @@ export default function Home() {
               <ScrollingText text="Projects" speed={10} direction="left" />
             </div>
           </div>
+
+          {/* Edukai */}
+          <Edukai />
         </div>
 
         {/* ContactSection */}
         <div
-          className="flex flex-col gap-2 mb-[5%] w-full pt-[5%] pb-[5%]"
+          className="flex flex-col gap-2 w-full pt-[5%] pb-[5%]"
           id="contactSection"
         >
           <div className="flex min-w-screen overflow-x-hidden">
@@ -184,39 +190,39 @@ export default function Home() {
               <ScrollingText text="Contact" speed={10} direction="left" />
             </div>
           </div>
-          <div className="mt-auto flex flex-col gap-2 w-full px-[15%] pt-[2%]">
-            <SectionTitle text="Contact me" />
-            <h3 className="text-primary text-lg md:text-2xl outfit-medium">
-              Feel free to contact me!
+          <div className="mt-auto flex flex-col gap-2 w-full px-[15%] pt-[2%] w-full">
+            {/* <SectionTitle text="Contact me" /> */}
+            <h3 className="text-2xl md:text-3xl outfit-bold text-primary px-[10%] md:px-[25%]">
+              Let me a message!
             </h3>
-            <div className="flex flex-wrap items-center justify-start gap-2 md:gap-5 mt-[2%] w-full">
+            <ContactForm />
+            <div className="px-[25%]">
+              <Separator text="or" className="my-4" />
+            </div>
+            <div className="flex flex-wrap items-center justify-center gap-2 md:gap-5 mt-[1%] w-full px-[10%] md:px-[25%]">
               {/* Malt */}
               <Button
                 variant={"outline"}
-                size={"lg"}
-                className="text-xl w-full md:w-auto border-[#2a17ff] hover:bg-[#2a17ff]/10 hover:text-[#2a17ff] selected:none outfit-medium text-lg text-primary"
+                size={"icon"}
+                className="text-xl md:w-auto border-[#e43d12] hover:bg-[#e43d12]/10 hover:text-[#e43d12] selected:none outfit-medium text-lg text-primary"
                 onClick={() =>
                   window.open("https://www.malt.fr/profile/tristanhourtoulle")
                 }
-                style={{
-                  filter: "drop-shadow(4px 4px 4px rgba(42, 23, 255, 0.5))",
-                }}
               >
                 <Image
                   src={MaltLogoWhite}
                   alt="Malt logo"
                   width={25}
                   height={25}
-                  className="mr-4"
+                  className=""
                 />
-                Malt.
               </Button>
 
               {/* Linkedin */}
               <Button
                 variant={"outline"}
-                size={"lg"}
-                className="text-xl w-full md:w-auto border-[#2a17ff] hover:bg-[#2a17ff]/10 hover:text-[#2a17ff] selected:none outfit-medium text-lg text-primary"
+                size={"icon"}
+                className="text-xl md:w-auto border-[#e43d12] hover:bg-[#e43d12]/10 hover:text-[#e43d12] selected:none outfit-medium text-lg text-primary"
                 onClick={() =>
                   window.open("https://www.linkedin.com/in/tristan-hourtoulle")
                 }
@@ -226,16 +232,15 @@ export default function Home() {
                   alt="LinkedIn logo"
                   width={25}
                   height={25}
-                  className="mr-4"
+                  className=""
                 />
-                Linkedin
               </Button>
 
               {/* Mail */}
               <Button
                 variant={"outline"}
-                size={"lg"}
-                className="text-xl w-full md:w-auto border-[#2a17ff] hover:bg-[#2a17ff]/10 hover:text-[#2a17ff] selected:none outfit-medium text-lg text-primary"
+                size={"icon"}
+                className="text-xl md:w-auto border-[#e43d12] hover:bg-[#e43d12]/10 hover:text-[#e43d12] selected:none outfit-medium text-lg text-primary"
                 onClick={() =>
                   window.open("mailto:contact@tristanhourtoulle.fr")
                 }
@@ -245,17 +250,34 @@ export default function Home() {
                   alt="Gmail logo"
                   width={25}
                   height={25}
-                  className="mr-4"
+                  className=""
                 />
-                Mail
+              </Button>
+
+              {/* Github */}
+              <Button
+                variant={"outline"}
+                size={"icon"}
+                className="text-xl md:w-auto border-[#e43d12] hover:bg-[#e43d12]/10 hover:text-[#e43d12] selected:none outfit-medium text-lg text-primary"
+                onClick={() =>
+                  window.open("https://github.com/TristanHourtoulle")
+                }
+              >
+                <Image
+                  src={GithubLogoOrange}
+                  alt="Github logo"
+                  width={30}
+                  height={30}
+                  className=""
+                />
               </Button>
             </div>
           </div>
         </div>
 
-        <div className="relative left-[40%] lg:left-[50%] animate-light-bounce-little w-10 h-10">
+        {/* <div className="relative left-[40%] lg:left-[50%] animate-light-bounce-little w-10 h-10">
           <UpArrow location="" />
-        </div>
+        </div> */}
       </div>
     </>
   );
