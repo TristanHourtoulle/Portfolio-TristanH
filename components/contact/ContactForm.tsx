dfc068df8218fef2a6976724032d9e474266a769
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-08-01 18:54:14 +0200

    feat: add contact page

--- components/contact/ContactForm.tsx
+++ components/contact/ContactForm.tsx
@@ -23,52 +23,6 @@ export const ContactForm = (props: ContactFormProps) => {
     <form
       onSubmit={onSubmit}
       className="flex flex-col items-center w-full mt-[2%] px-[10%] md:px-[25%]"
-    >
-      {/* Mail */}
-      <div className="grid w-full items-center gap-1.5 outfit-regular mb-3 w-full">
-        <Label htmlFor="email">Email</Label>
-        <Input
-          type="email"
-          id="email"
-          placeholder="Email"
-          className="border border-[#e43d12]/15"
-        />
-      </div>
-
-      {/* Title of the mail */}
-      <div className="grid w-full items-center gap-1.5 outfit-regular mb-3">
-        <Label htmlFor="title">Title</Label>
-        <Input
-          type="text"
-          id="title"
-          placeholder="Title"
-          className="border border-[#e43d12]/15"
-        />
-      </div>
-
-      {/* Content of the mail */}
-      <div className="grid w-full items-center gap-1.5 outfit-regular mb-3">
-        <Label htmlFor="content">Content</Label>
-        <Textarea
-          id="content"
-          placeholder="Type your message here..."
-          className="border border-[#e43d12]/15 p-2"
-        />
-      </div>
-
-      <Button
-        size={"lg"}
-        type="submit"
-        disabled={isLoading}
-        className="transition-all bg-[#e43d12] hover:bg-[#e43d12]/70 text-lg lg:text-xl outfit-medium w-full"
-      >
-        {isLoading ? (
-          <LoaderCircle className="animate-spin mr-4" />
-        ) : (
-          <SendHorizontal className="mr-4" />
-        )}
-        {isLoading ? "Sending..." : "Send"}
-      </Button>
-    </form>
+    ></form>
   );
 };
