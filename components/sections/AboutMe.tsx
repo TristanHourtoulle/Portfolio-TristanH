09e6248e456156c1c24cc0c6400f405cc730e3df
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-12-01 16:46:31 +0800

    feat: add animation in sections content

--- components/sections/AboutMe.tsx
+++ components/sections/AboutMe.tsx
@@ -4,14 +4,41 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 
 export const AboutMeSection = () => {
-  // Variants pour les items
+  // Variants pour les animations générales
+  const fadeIn = {
+    hidden: { opacity: 0, y: 30 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.6, ease: "easeOut" },
+    },
+  };
+
+  const fadeUp = {
+    hidden: { opacity: 0, y: 40 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.8, ease: "easeOut" },
+    },
+  };
+
+  const imageVariants = {
+    hidden: { opacity: 0, scale: 0.8 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: { duration: 0.8, ease: "easeOut" },
+    },
+  };
+
   const itemVariants = {
-    hidden: { opacity: 0, y: 20 }, // Position initiale
+    hidden: { opacity: 0, y: 20 },
     visible: (index: any) => ({
       opacity: 1,
       y: 0,
       transition: {
-        delay: index * 0.2, // Délai pour chaque item
+        delay: index * 0.2,
         duration: 0.6,
         ease: "easeOut",
       },
@@ -19,27 +46,45 @@ export const AboutMeSection = () => {
   };
 
   return (
-    <div className="py-6 lg:py-0 flex flex-col items-center justify-center gap-3 lg:gap-8">
+    <motion.div
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
+      className="py-6 lg:py-0 flex flex-col items-center justify-center gap-3 lg:gap-8"
+    >
       {/* Title */}
-      <h1 className="text-[#4F62CC] font-bold text-xl lg:text-4xl mb-[7%]">
+      <motion.h1
+        className="text-[#4F62CC] font-bold text-xl lg:text-4xl mb-[7%]"
+        variants={fadeUp}
+      >
         Qui suis-je réellement ?
-      </h1>
+      </motion.h1>
       {/* Content */}
       <div className="flex flex-col lg:flex-row items-center gap-[5%]">
-        <Image
-          src="/photos/tristan2.png"
-          alt="Tristan Hourtoulle"
-          width={400}
-          height={400}
-          className="rounded-md"
-        />
-        {/* Right Side */}
-        <div className="flex flex-col items-center lg:items-start justify-between gap-3 lg:gap-1 w-full ">
-          <h2 className="mt-3 lg:mt-0 text-xl text-center lg:text-left lg:text-4xl text-[#1E1E1E] font-semibold mb-2">
-            Tristan Hourtoulle
-          </h2>
+        {/* Image */}
+        <motion.div variants={imageVariants}>
+          <Image
+            src="/photos/tristan2.png"
+            alt="Tristan Hourtoulle"
+            width={400}
+            height={400}
+            className="rounded-md"
+          />
+        </motion.div>
 
-          <p className="font-regular text-sm lg:text-md text-[#1E1E1E] w-full lg:max-w-2xl mb-1">
+        {/* Right Side */}
+        <div className="flex flex-col items-center lg:items-start justify-between gap-3 lg:gap-1 w-full">
+          <motion.h2
+            className="mt-3 lg:mt-0 text-xl text-center lg:text-left lg:text-4xl text-[#1E1E1E] font-semibold mb-2"
+            variants={fadeIn}
+          >
+            Tristan Hourtoulle
+          </motion.h2>
+
+          <motion.p
+            className="font-regular text-sm lg:text-lg text-[#1E1E1E] w-full lg:max-w-2xl mb-1"
+            variants={fadeIn}
+          >
             Je suis développeur <span className="font-bold">front-end</span> et{" "}
             <span className="font-bold">designer web</span>, basé en{" "}
             <span className="font-bold">Malaisie</span>, spécialisé dans la
@@ -54,68 +99,43 @@ export const AboutMeSection = () => {
             <span className="font-bold">React</span>,{" "}
             <span className="font-bold">Next.js</span>, et{" "}
             <span className="font-bold">Figma</span>, permettant de concevoir
-            des solutions intuitives et performantes. Basé à l'international, je
-            collabore avec des clients du monde entier pour donner vie à leurs
-            visions numériques avec efficacité et créativité.
-          </p>
-          <p className="font-regular text-md text-[#4F62CC] max-w-lg mb-4">
+            des solutions intuitives et performantes.
+          </motion.p>
+
+          <motion.p
+            className="font-regular text-md lg:text-lg text-[#4F62CC] max-w-lg mb-4"
+            variants={fadeIn}
+          >
             Je crois que chaque projet mérite une attention particulière et un
             design d'exception, rendant l'utile toujours agréable.
-          </p>
+          </motion.p>
 
           <hr className="w-full border-[1.5px] border-[#4F62CC] border-opacity-75 rounded-full" />
           <div className="flex flex-col lg:flex-row items-center justify-center lg:justify-start gap-3 lg:gap-[10%] w-full lg:my-2">
-            {/* Item 1 */}
-            <motion.div
-              className="flex flex-col gap-0 items-center lg:items-start w-full"
-              custom={0} // Index pour le délai d'animation
-              initial="hidden"
-              animate="visible"
-              variants={itemVariants}
-            >
-              <p className="text-black text-[#1E1E1E] text-3xl">
-                15<span className="text-[#4F62CC]">+</span>
-              </p>
-              <p className="text-[#1E1E1E] text-opacity-75 text-md">
-                Projets complétés
-              </p>
-            </motion.div>
-
-            {/* Item 2 */}
-            <motion.div
-              className="flex flex-col gap-0 items-center lg:items-start w-full"
-              custom={1} // Index pour le délai d'animation
-              initial="hidden"
-              animate="visible"
-              variants={itemVariants}
-            >
-              <p className="text-black text-[#1E1E1E] text-3xl">
-                12<span className="text-[#4F62CC]">+</span>
-              </p>
-              <p className="text-[#1E1E1E] text-opacity-75 text-md">
-                Clients satisfaits
-              </p>
-            </motion.div>
-
-            {/* Item 3 */}
-            <motion.div
-              className="flex flex-col gap-0 items-center lg:items-start w-full"
-              custom={2} // Index pour le délai d'animation
-              initial="hidden"
-              animate="visible"
-              variants={itemVariants}
-            >
-              <p className="text-black text-[#1E1E1E] text-3xl">
-                03<span className="text-[#4F62CC]">+</span>
-              </p>
-              <p className="text-[#1E1E1E] text-opacity-75 text-md">
-                Années d’expérience
-              </p>
-            </motion.div>
+            {/* Items */}
+            {[
+              "Projets complétés",
+              "Clients satisfaits",
+              "Années d’expérience",
+            ].map((text, index) => (
+              <motion.div
+                key={text}
+                className="flex flex-col gap-0 items-center lg:items-start w-full"
+                custom={index}
+                initial="hidden"
+                animate="visible"
+                variants={itemVariants}
+              >
+                <p className="text-black text-[#1E1E1E] text-3xl">
+                  {index === 0 ? "15+" : index === 1 ? "12+" : "03+"}
+                </p>
+                <p className="text-[#1E1E1E] text-opacity-75 text-md">{text}</p>
+              </motion.div>
+            ))}
           </div>
           <hr className="w-full border-[1.5px] border-[#4F62CC] border-opacity-75 rounded-full" />
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
